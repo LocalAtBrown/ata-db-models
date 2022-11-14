bbad653955ae6e6252fab42fe731d53a355f0c3f
Raaid <raaid@protonmail.com>
2022-11-14 13:44:38 -0500

    add some type annotations

--- src/helpers.py
+++ src/helpers.py
@@ -3,16 +3,16 @@ import os
 from sqlmodel import Session, create_engine
 
 
-engine = create_engine(os.getenv("DB_CONNECTION_STRING"))
+engine = create_engine(os.getenv("DB_CONNECTION_STRING", "postgresql://postgres:postgres@localhost:5432/postgres"))
 
 
 def get_session() -> Session:
     return Session(engine, autocommit=False, autoflush=False)
 
 
-def create_databases():
+def create_databases() -> None:
     pass
 
 
-def create_users():
+def create_users() -> None:
     pass

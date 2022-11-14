ab8e18dcdea11af3ea13ca373c33b7ce8afbadd4
Raaid <raaid@protonmail.com>
2022-11-14 15:57:27 -0500

    start filling out helper functions

--- src/helpers.py
+++ src/helpers.py
@@ -1,18 +1,27 @@
 import os
 
-from sqlmodel import Session, create_engine
+from sqlmodel import create_engine, text
+
+engine = create_engine(
+    os.getenv(
+        "DB_CONNECTION_STRING", "postgresql://postgres:postgres@localhost:5432/postgres"
+    )
+)
 
 
-engine = create_engine(os.getenv("DB_CONNECTION_STRING", "postgresql://postgres:postgres@localhost:5432/postgres"))
+def create_database(db_name: str) -> None:
+    statement = text("CREATE DATABASE :db_name")
+    with engine.connect() as conn:
+        conn.execute(statement, {"db_name": db_name})
+        conn.commit()
 
 
-def get_session() -> Session:
-    return Session(engine, autocommit=False, autoflush=False)
-
-
-def create_databases() -> None:
-    pass
-
-
-def create_users() -> None:
-    pass
+def create_user(user_name: str) -> None:
+    # create a user per component per partner per stage (per stage corresponds to per db)
+    # for example: afrola should have 1 for pipeline0, one for pipeline1, and one for the api, all with appropriate
+    # permissions (p0  reads/writes to events, p1 reads from events and reads/writes to X, api only reads X)
+    # and this would all be for one stage, e.g. prod
+    statement = text("CREATE USER :user_name WITH PASSWORD ':password'")
+    with engine.connect() as conn:
+        conn.execute(statement, {"user_name": user_name, "password": "todo"})
+        conn.commit()

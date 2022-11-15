185b8fb535c5a67f38aeb64c19fa0370833bef72
Raaid <raaid@protonmail.com>
2022-11-15 16:23:42 -0500

    change to simple f-strings, binding params is for sql expressions where the thing needs to be in single quotes or a param to a where clause etc.

--- src/helpers.py
+++ src/helpers.py
@@ -26,25 +26,29 @@ class Privilege(str, Enum):
 
 
 def create_database(conn: Connection, db_name: str) -> None:
-    statement = text("CREATE DATABASE :db_name")
-    conn.execute(statement, {"db_name": db_name})
+    statement = text(f"CREATE DATABASE {db_name}")
+    conn.execute(statement)
 
 
 def create_user(conn: Connection, user_name: str) -> None:
-    statement = text("CREATE USER :user_name WITH PASSWORD ':password'")
-    conn.execute(statement, {"user_name": user_name, "password": "todo"})
+    statement = text(f"CREATE USER {user_name} WITH PASSWORD :password")
+    conn.execute(statement, {"password": "todo"})
 
 
 def grant_privileges(
     conn: Connection, user_name: str, table: str, privileges: list[Privilege]
 ) -> None:
     formatted_privileges = " ".join(privileges)
-    statement = text(f"GRANT :formatted_privileges ON :table TO :user_name")
-    conn.execute(
-        statement,
-        {
-            "formatted_privileges": formatted_privileges,
-            "table": table,
-            "user_name": user_name,
-        },
+    statement = text(f"GRANT {formatted_privileges} ON {table} TO {user_name}")
+    conn.execute(statement)
+
+
+def enable_row_level_security(
+    conn: Connection, table: str, target_column: str, user_name: str
+) -> None:
+    s1 = text(f"ALTER TABLE {table} ENABLE ROW LEVEL SECURITY")
+    conn.execute(s1)
+    s2 = text(
+        f"CREATE POLICY TODO ON {table} TO {user_name} USING (current_user LIKE '%' || {target_column} || '%')"
     )
+    conn.execute(s2)

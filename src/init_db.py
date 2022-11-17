aff317bc334a1f0a48b4de5af992bf8197f9e417
Raaid <raaid@protonmail.com>
2022-11-17 12:09:18 -0500

    add function to create conn string, use in appropriate spots

--- src/init_db.py
+++ src/init_db.py
@@ -1,3 +1,5 @@
+import os
+
 from sqlalchemy.future.engine import create_engine
 
 from src.helpers import (
@@ -14,10 +16,11 @@ from src.helpers import (
     enable_row_level_security,
     grant_privileges,
 )
+from src.models import SQLModel
 
 
 def pre_table_initialization(stage: Stage, components: list[Component], partner_names: list[Partner]) -> None:
-    engine = create_engine(f"something something with admin access to default db")
+    engine = create_engine(get_conn_string(db_name="postgres"))
 
     with engine.connect() as conn:
         create_database(conn, db_name=stage)
@@ -27,16 +30,17 @@ def pre_table_initialization(stage: Stage, components: list[Component], partner_
             usernames = [f"{stage}_{component.name}_{partner_name}" for partner_name in partner_names]
             create_users(conn, usernames=usernames)
             assign_role(conn, role=role, usernames=usernames)
-            # TODO store creds per user in SSM
+            # TODO store creds per user in SSM (db, host, port, usr, pw) or conn_string
         conn.commit()
 
 
-def initialize_tables(stage: str) -> None:
-    pass
+def initialize_tables(stage: Stage) -> None:
+    engine = create_engine(get_conn_string(db_name=stage))
+    SQLModel.metadata.create_all(engine)
 
 
 def post_table_initialization(stage: Stage, components: list[Component]) -> None:
-    engine = create_engine(f"something with admin to THIS STAGE'S DB since we have to refer to tables")
+    engine = create_engine(get_conn_string(db_name=stage))
 
     with engine.connect() as conn:
         for component in components:
@@ -54,12 +58,22 @@ def post_table_initialization(stage: Stage, components: list[Component]) -> None
 
 
 def initialize_all_database_entities(stage: Stage, components: list[Component], partner_names: list[Partner]) -> None:
-    # needs full admin
+    # needs default db
     pre_table_initialization(stage=stage, components=components, partner_names=partner_names)
     # needs to connect to this stage's db
     initialize_tables(stage=stage)
-    # needs full admin
+    # needs this stage's db
     post_table_initialization(stage=stage, components=components)
+
+
+def get_conn_string(db_name: str) -> str:
+    # everything but dbname should be the same, since we are using the admin user for everything
+    host = os.getenv("HOST", "localhost")
+    port = os.getenv("PORT", "5432")
+    user = os.getenv("USERNAME", "postgres")
+    password = os.getenv("PASSWORD", "postgres")
+
+    return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
 
 
 def main():

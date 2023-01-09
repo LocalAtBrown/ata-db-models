83a21e96654c831fe661c5be1896e4080ebf4e6f
Raaid <raaid@protonmail.com>
2023-01-09 11:28:22 -0500

    update alembic env.py to work with SQLModel and make it easy to pass a DB conn string to it

--- alembic/env.py
+++ alembic/env.py
@@ -1,12 +1,15 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config, pool
 
 from alembic import context
+from ata_db_models.models import SQLModel
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", os.getenv("DB_CONNECTION_STRING"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -17,7 +20,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -43,6 +46,7 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        include_schemas=True,
     )
 
     with context.begin_transaction():

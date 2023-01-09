c51e5d65f1e0874ccc1ce369ed0d1959278f804a
Raaid <raaid@protonmail.com>
2023-01-09 14:33:52 -0500

    add default to os.getenv call

--- alembic/env.py
+++ alembic/env.py
@@ -21,6 +21,7 @@ if config.config_file_name is not None:
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
+# 718 780 5878
 # target_metadata = mymodel.Base.metadata
 target_metadata = SQLModel.metadata
 
